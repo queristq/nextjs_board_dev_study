c4c1545f369e9acc09257df985fdd280dd766337
Youngmok Yun <mok@contoro.com>
2023-06-25 16:02:05 -0500

    The case of no-log-in has been considered

--- pages/api/post/delete.js
+++ pages/api/post/delete.js
@@ -7,11 +7,15 @@ export default async function handler(요청, 응답) {
     if (요청.method == 'POST'){
         // console.log(요청.body)
         let session = await getServerSession(요청, 응답, authOptions)
-
+        
         const db = (await connectDB).db('forum')
         let 찾은거 = await db.collection('post').findOne({ _id : new ObjectId(요청.body) })
-    
-        if (찾은거.author == session.user.email) {
+        
+        if(!session){
+          return 응답.status(500).json('현재유저와 작성자 불일치')
+        }
+
+        if (찾은거.author == session.user.email || 찾은거.author == null) {
           let result = await db.collection('post').deleteOne({ _id : new ObjectId(요청.body) })
           return 응답.status(200).json('삭제완료')
         } else {
